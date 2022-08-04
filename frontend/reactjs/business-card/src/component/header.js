17db59749b71b18c0b424bbb50c5cb52a56ce469
unknown <gerypratama.o@gmail.com>
2022-08-04 13:33:22 +0700

    updated business card buttons and links

--- frontend/reactjs/business-card/src/component/header.js
+++ frontend/reactjs/business-card/src/component/header.js
@@ -1,15 +1,21 @@
 import './style.css'
 import Photo from '../assets/img/profile.jpg'
+import Mail from '../assets/ico/mail.png'
 
 const Header = () => (
     <div className="profile-header">
         <img src={Photo} alt="Profile" />
         <h2 className="profile-name">Gery Pratama</h2>
         <p className="profile-title">Frontend Developer</p>
-        <div className="buttons-container">
-            <button type="button" id="email">Email</button>
-            <button type="button" id="linkedin">LinkedIn</button>
-        </div>
+        <button
+            type="button"
+            id="email"
+            onClick={(e) => {
+                window.location.href = 'mailto:gerypratama.o@gmail.com';
+                e.preventDefault();
+            }}>
+            <img src={Mail} alt="Email" />Email
+        </button>
     </div>
 )
 
--- frontend/reactjs/business-card/src/component/icons.js
+++ frontend/reactjs/business-card/src/component/icons.js
@@ -1,13 +1,19 @@
 import './style.css';
-import Facebook from '../assets/ico/facebook.png'
 import GitHub from '../assets/ico/github.png'
 import Instagram from '../assets/ico/instagram.png'
+import Linkedin from '../assets/ico/linkedin.png'
 
 const SocialIcons = () => (
     <div className="soc-med-bar">
-        <img src={Facebook} alt="Facebook" />
-        <img src={GitHub} alt="Facebook" />
-        <img src={Instagram} alt="Facebook" />
+        <a href='https://github.com/gerypratama' target='_blank' rel='noreferrer'>
+            <img src={GitHub} alt="GitHub" />
+        </a>
+        <a href='https://www.instagram.com/gery.oktav/' target='_blank' rel='noreferrer'>
+            <img src={Instagram} alt="Instagram" />
+        </a>
+        <a href='https://www.linkedin.com/in/gery-pratama-oktaviano/' target='_blank' rel='noreferrer'>
+            <img src={Linkedin} alt="LinkedIn" />
+        </a>
     </div>
 )
 
